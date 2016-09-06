1ba23e766d8e20b7f3dc949c331f236aa4742399
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2016-09-06 19:40:39 -0300

    Controller da Tela de Login

--- app/src/main/java/com/lm/habitatapp/HomeActivity.java
+++ app/src/main/java/com/lm/habitatapp/HomeActivity.java
@@ -1,13 +1,39 @@
 package com.lm.habitatapp;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends Activity {
+
+    private EditText edtusuario, edtSenha;
+    private Button btnEntrar, btnNovoCadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnNovoCadastro = (Button)findViewById(R.id.btnNovoCadastro);
+        novoCadastro();
+    }
+
+    public void novoCadastro(){
+        btnNovoCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private Context getContext(){
+        return this;
     }
 }
