5c6d0bc3e6fbcb37710b02628ed12e39351ce760
Lucas Albuquerque <albuquerque.r.lucas@gmail.com>
2016-09-06 19:40:58 -0300

    Controller da Tela Splash

---
+++ app/src/main/java/com/lm/habitatapp/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.lm.habitatapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SplashActivity extends Activity {
+
+    private static final String ERR = "Erro";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(4000);
+                }catch (InterruptedException e){
+                    Log.e(ERR, "Erro na execução da Thread.");
+                    e.printStackTrace();
+                }finally {
+                    Intent intent = new Intent(getContext(), HomeActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+
+    private Context getContext(){
+        return this;
+    }
+}
